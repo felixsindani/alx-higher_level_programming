7d33bb72b0a1821f4bcb068060894d807a75344e
felixsindani <felixsindani72@gmail.com>
2023-08-13 18:54:23 +0000

    added hash bang

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 if __name__ == "__main__":
     """imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3"""
     from add_0 import add
