62edbed30cafc2b4d3c5f6dfdd6763dd6fd2b1b8
felixsindani <felixsindani72@gmail.com>
2023-08-13 18:25:32 +0000

    fix betty

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-	"""imports all functions from the file calculator_1.py and handles basic operations"""
-from calculator_1 import add, sub, mul, div 
+	"""imports functions from file calculator_1.py do basic operations"""
+from calculator_1 import add, sub, mul, div
 import sys
 
 if len(sys.argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
 ops = {"+": add, "-": sub, "*": mul, "/": div}
 if sys.argv[2] not in list(ops.keys()):
-        print("Unknown operator. Available operators: +, -, * and /")
+print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
 a = int(sys.argv[1])
