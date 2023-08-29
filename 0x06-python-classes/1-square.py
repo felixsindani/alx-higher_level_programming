752a5c8d9e980f638f1bc4279cff67ce55e7f8f8
felixsindani <felixsindani72@gmail.com>
2023-08-29 10:56:12 -0700

    task 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -2,8 +2,9 @@
 """class Square that defines a square"""
 class Square:
     """Square representation"""
-
-    Args:
+    def __init__(self, size):
+        """Initialize a new square
+        Args:
         size (init): Size of new square
         """
         self.__size = size
